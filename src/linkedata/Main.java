3fb6951705893002911d64780740251e8eb15ed7
kdaud <dkakumirizii@gmail.com>
2020-11-22 17:52:24 +0300

    javadocs comment' '

--- src/linkedata/Main.java
+++ src/linkedata/Main.java
@@ -4,6 +4,9 @@ import datastructure.MyTec;
 
 public class Main {
 	
+	static {
+		System.out.println("My Programming Journey!");
+	}
 	public static void main(String[] args) {
 		MyTec obj = new MyTec();
 		obj.mycommit("Programming is life changing");
